f525032ee2d4a2b99267575488ae00d64f567c15
Jaffar Abbas <gamaportal8@gmail.com>
2020-07-07 13:00:15 +0500

    7-7-2020

---
+++ first.java
@@ -0,0 +1,17 @@
+
+class check{
+    void c(){
+        try {
+            Float f1 = new 
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
+
+
+class first{
+    public static void main(String[] args) {
+        
+    }
+}
